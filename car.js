276a747670818997c935be70110e153820f0f6d7
Praphul <praphulkumar2023@gmail.com>
2023-08-04 15:38:41 +0530

    Export module in NodeJS

---
+++ car.js
@@ -0,0 +1,9 @@
+exports.ford = {
+    brand: "Ford",
+    model: "Mustang"
+}
+
+exports.toyota = {
+    brand: "Toyota",
+    model: "Supra"
+}
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-console.log("Hello World!!");
+// console.log("Hello World!!");
 
-console.log("First tutorial of NOde js ");
-console.log("I will learn Node.js in a weak");
+// console.log("First tutorial of NOde js ");
+// console.log("I will learn Node.js in a weak");
 
 // Programatically Terminate a Node Program
 // process.exit(1);
 // process.exitCode = 1;
+
+// Using Exports in JS
+
+const { ford, toyota } = require('./car');
+console.log(ford);
+
+// Print Object using JSON.stringify()
+console.log(JSON.stringify(ford, null, 2));
+console.log(JSON.stringify(toyota, null, 4));
