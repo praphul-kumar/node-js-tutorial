3a54357664ffe5c35d7ae5e0ee547f1633862307
Praphul <praphulkumar2023@gmail.com>
2023-08-04 13:05:17 +0530

    Programatically terminate a program

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ console.log("Hello World!!");
 console.log("First tutorial of NOde js ");
 console.log("I will learn Node.js in a weak");
 
+// Programatically Terminate a Node Program
+// process.exit(1);
+// process.exitCode = 1;
